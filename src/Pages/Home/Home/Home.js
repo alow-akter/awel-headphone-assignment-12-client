ee90907f2e2421598a3e57fa75eaf1d38eca87ff
alow-akter <kironalow453@gmail.com>
2022-11-24 11:38:19 -0500

    navber commit

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import HomeSilder from '../HomeSilder/HomeSilder';
+import HeadphoneInfo from './HeadphoneInfo/HeadphoneInfo';
 
 const Home = () => {
     return (
         <div>
             <HomeSilder></HomeSilder>
+            <HeadphoneInfo></HeadphoneInfo>
         </div>
     );
 };
