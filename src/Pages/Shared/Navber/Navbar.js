e95e56991d889e25d141673adb2bd595be805e85
alow-akter <kironalow453@gmail.com>
2022-11-25 10:05:48 -0500

    sing out part commit

--- src/Pages/Shared/Navber/Navbar.js
+++ src/Pages/Shared/Navber/Navbar.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../Context/AuthProvider';
 
 const Navbar = () => {
+
+    const { user, logOut } = useContext(AuthContext)
+    const handleLogout = () => {
+        logOut()
+            .then(() => { })
+            .catch(err => console.log(err))
+    }
+
     const menuItems = <React.Fragment>
 
         <li><Link to='/'>Home</Link></li>
         <li><Link to='/blog'>Blog</Link></li>
         <li><Link to='/login'>Login</Link></li>
+
+        {
+            user?.uid ?
+                <>
+
+                    <li><button onClick={handleLogout}>Sing Out</button></li>
+                </>
+                : <li><Link to='/login'>Login</Link></li>
+        }
+
     </React.Fragment>
     return (
         <div>
